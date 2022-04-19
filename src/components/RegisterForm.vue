6b80294cf40f718d9251b65331fb9eab62da8e8c
gdkimaiyo <gdkimaiyo@gmail.com>
2022-04-19 23:59:02 +0300

    chore: add register page design

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="register-form">
+    <h3>Register Form</h3>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RegisterForm",
+});
+</script>
+
+<style lang="scss" scoped>
+@media only screen and (max-width: 575px) {
+  //
+}
+</style>
