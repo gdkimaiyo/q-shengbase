19bd4f4f107a5c853e99b9ebd4093c6dbab3c19d
gdkimaiyo <gdkimaiyo@gmail.com>
2022-04-21 00:33:23 +0300

    ft: successfully register

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -11,13 +11,13 @@
           </p>
         </div>
         <div class="register-form">
-          <h5 class="logo-btn q-mt-none" @click="goHome()">ShengBase</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-            molestiae maxime unde harum quidem nobis, alias, voluptatum cumque
-            blanditiis tempore nostrum? Libero eos magnam dolores similique, est
-            delectus quia modi? HIGH SCHOOL
-          </p>
+          <h5 class="logo-btn href-link q-my-none q-pb-md" @click="navigateTo('/')">ShengBase</h5>
+          <div class="text-h5 q-pb-sm text-weight-bold">Create your Account</div>
+          <div class="text-subtitle2 q-pb-md text-weight-bold">
+            Already have an account?
+            <a class="href-link" @click="navigateTo('/login')">Sign In</a>
+          </div>
+          <RegisterForm />
         </div>
       </div>
     </div>
@@ -26,14 +26,19 @@
 
 <script>
 import { defineComponent } from "vue";
+import RegisterForm from "../components/RegisterForm.vue";
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Register",
 
+  components: {
+    RegisterForm,
+  },
+
   methods: {
-    goHome() {
-      this.$router.push("/");
+    navigateTo(route) {
+      this.$router.push(route);
     },
   },
 });
@@ -45,13 +50,15 @@ export default defineComponent({
   max-width: 960px;
   margin: 0 auto;
 }
-.logo-btn {
-  font-size: 1.7rem;
-  font-weight: bold;
+.href-link {
   color: #0c45b0;
   cursor: pointer;
 }
-.logo-btn:hover {
+.logo-btn {
+  font-size: 1.7rem;
+  font-weight: bold;
+}
+.href-link:hover {
   color: rgba(12, 69, 176, 0.85);
 }
 
@@ -65,6 +72,7 @@ export default defineComponent({
   // box-shadow: 0px 1px 9px 2px rgba(0, 0, 0, 0.15);
 }
 .register-info {
+  width: 45%;
   display: block;
   background-image: url("../assets/sky.png");
   background-repeat: no-repeat;
@@ -79,6 +87,7 @@ export default defineComponent({
   }
 }
 .register-form {
+  width: 55%;
   margin-left: 32px;
   padding: 24px;
 }
