d1757c57e6fdc1893c6fdfa6c5c80daa51dc07cc
gdkimaiyo <gdkimaiyo@gmail.com>
2022-04-22 00:27:12 +0300

    ft: add registration faqs

---
+++ src/utils/contants.js
@@ -0,0 +1,21 @@
+export const REGISTER_FAQS = [
+  {
+    id: 1,
+    faq: "What is ShengBase Trivia?",
+    desc: "<b>ShengBase Trivia</b> is a set of quizzes presented to Users once per week. The quizzes will be mostly from ShengBase <b>dictionary</b> or any category deemed fit by ShengaBase management.",
+    desc2:
+      "Participating in <b>ShengBase Trivia</b> and correctly answering quizzes earns you points which can be redeemed.",
+  },
+  {
+    id: 2,
+    faq: "What benefits will I get when I add a sheng word.",
+    desc: "<b>First</b>, you will contribute in enriching our ShengBase dictionary. All words you add will have your name as the <b>Author</b>.",
+    desc2:
+      "<b>Lastly</b>, other users will vote for a sheng word(s) you add to our ShengBase dictionary. Voted words earn points which can be redeemed.",
+  },
+  {
+    id: 3,
+    faq: "Why should I use my email address?",
+    desc: "Your <b>email address</b> will be used to uniquely identify yourself. Also email address by default will be your <b>username</b>. We will never share your email address.",
+  },
+];
