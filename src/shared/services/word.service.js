a6e6d20ee48c116a6292db8ac91da01d64db0ddc
gdkimaiyo <gdkimaiyo@gmail.com>
2022-05-18 22:52:01 +0300

    chore: chow total number of sheng words in the dictionary

--- src/shared/services/word.service.js
+++ src/shared/services/word.service.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { BACKEND_API } from "../../secrets.config";
 
+// Get all words
+export const getWords = async () => {
+  const response = await axios.get(`${BACKEND_API}/api/v1/words`);
+  return response.data;
+};
+
 // Add a word
 export const addNewWord = async (data) => {
   const response = await axios.post(`${BACKEND_API}/api/v1/addword`, data);
