1f1a02f769d0c9ff1284f9fd85fb20168c263a45
gdkimaiyo <gdkimaiyo@gmail.com>
2022-06-18 16:29:26 +0300

    ft: add faqs page design

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,3 +1,30 @@
+export const FAQS = [
+  {
+    id: 1,
+    open: true,
+    faq: "What is ShengBase Trivia?",
+    desc: "<b>ShengBase Trivia</b> is a set of quizzes presented to Users once per week. The quizzes will be mostly from ShengBase <b>dictionary</b> or any category deemed fit by ShengaBase management.",
+    desc2:
+      "Participating in <b>ShengBase Trivia</b> and correctly answering quizzes earns you points which can be redeemed.",
+  },
+  {
+    id: 2,
+    open: false,
+    faq: "What benefits will I get when I add a sheng word.",
+    desc: "<b>First</b>, you will contribute in enriching our ShengBase dictionary. All words you add will have your name as the <b>Author</b>.",
+    desc2:
+      "<b>Lastly</b>, other users will vote for a sheng word(s) you add to our ShengBase dictionary. Voted words earn points which can be redeemed.",
+  },
+  {
+    id: 3,
+    open: false,
+    faq: "What is ShengBase Trivia?",
+    desc: "<b>ShengBase Trivia</b> is a set of quizzes presented to Users once per week. The quizzes will be mostly from ShengBase <b>dictionary</b> or any category deemed fit by ShengaBase management.",
+    desc2:
+      "Participating in <b>ShengBase Trivia</b> and correctly answering quizzes earns you points which can be redeemed.",
+  },
+];
+
 export const REGISTER_FAQS = [
   {
     id: 1,
