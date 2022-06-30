cb094a6e6ce59adab91f27cc6901a587b00feb17
gdkimaiyo <gdkimaiyo@gmail.com>
2022-06-30 10:14:19 +0300

    chore: update faqs

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -2,26 +2,24 @@ export const FAQS = [
   {
     id: 1,
     open: true,
+    faq: "What is ShengBase?",
+    desc: "<b>ShengBase</b> brings you the Sheng Dictionary enriched with latest and old sheng words. The words are added by ShengBase users from different regions in Kenya.",
+  },
+  {
+    id: 2,
+    open: false,
     faq: "What is ShengBase Trivia?",
-    desc: "<b>ShengBase Trivia</b> is a set of quizzes presented to Users once per week. The quizzes will be mostly from ShengBase <b>dictionary</b> or any category deemed fit by ShengaBase management.",
+    desc: "<b>ShengBase Trivia</b> is a set of quizzes presented to users once per week. The quizzes will be mostly from ShengBase <b>dictionary</b> or any category deemed fit by ShengaBase management.",
     desc2:
       "Participating in <b>ShengBase Trivia</b> and correctly answering quizzes earns you points which can be redeemed.",
   },
   {
-    id: 2,
+    id: 3,
     open: false,
     faq: "What benefits will I get when I add a sheng word.",
     desc: "<b>First</b>, you will contribute in enriching our ShengBase dictionary. All words you add will have your name as the <b>Author</b>.",
     desc2:
       "<b>Lastly</b>, other users will vote for a sheng word(s) you add to our ShengBase dictionary. Voted words earn points which can be redeemed.",
-  },
-  {
-    id: 3,
-    open: false,
-    faq: "What is ShengBase Trivia?",
-    desc: "<b>ShengBase Trivia</b> is a set of quizzes presented to Users once per week. The quizzes will be mostly from ShengBase <b>dictionary</b> or any category deemed fit by ShengaBase management.",
-    desc2:
-      "Participating in <b>ShengBase Trivia</b> and correctly answering quizzes earns you points which can be redeemed.",
   },
 ];
 
